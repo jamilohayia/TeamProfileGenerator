bf15700187795dde880377c7dfed430c1adf9c45
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-10-11 21:42:19 -0400

    added functionality to the app.js file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./Develop/lib/Manager");
+const Engineer = require("./Develop/lib/Engineer");
+const Intern = require("./develop/lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,137 +10,134 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-function generateEmployeeInfo(){
+employeeInformation = () => {
     inquirer.prompt([
         {
-            message: "How many Managers in your team?",
-            name: "numOfManagers",
-            type: "number"
+            type: "list",
+            message: "What type of employee?",
+            name: "name",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+                "Team Completed"
+            ]
+        },
+    ]).then(response => {
+        if (response.name === "Manager") {
+            managerInformation();
+        } else if (response.name === "Engineer") {
+            engineerInformation();
+        } else if (response.name === "Intern") {
+            internInformation();
+        } else if (response.name === "Team Completed") {
+            generateHTML(outputPath, render(team));
+        };
+    });
+};
+
+managerInformation = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your manager's name?",
+            name: "name"
         },
         {
-            message: "How many Interns in your team?",
-            name: "numOfInterns",
-            type: "number"
+            type: "input",
+            message: "What is your manager's ID?",
+            name: "id"
         },
         {
-            message: "How many Engineers in your team?",
-            name: "numOfEngineers",
-            type: "number"
+            type: "input",
+            message: "What is your manager's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your manager's office number?",
+            name: "number"
+        },
+    ]).then(function (answer) {
+        let manager = new Manager(answer.name, answer.id, answer.email, answer.number);
+        team.push(manager);
+        console.log(`Manager is : ${manager.name}`);
+        employeeInformation();
+    })
+};
+
+engineerInformation= () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your engineer's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your engineer's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your engineer's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your engineer's Github username?",
+            name: "github"
         }
-    ]).then(answers => {
-        createEmployeeInfo([], answers);
+    ]).then(function (answer) {
+        let engineer = new Engineer(answer.name, answer.id, answer.email, answer.github)
+        team.push(engineer);
+
+        employeeInformation()
+    })
+};
+
+internInformation= () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "what is your intern's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your intern's ID",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What is your intern's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your intern's school",
+            name: "school",
+        },
+    ]).then(function (answer) {
+        let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
+        team.push(intern);
+
+        employeeInformation()
+    })
+};
+
+//file generateHTML(outputPath, render(team));
+generateHTML = (fileName, data) => {
+    fs.writeFile(fileName, data, "utf8", function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log("Successfully assembled My Team");
     });
-}
+};
 
-function  createEmployeeInfo(employeeInfo, numOfEmployees){
-    if (employeeInfo.length < numOfEmployees.numOfManagers){
-        createManager(employeeInfo, numOfEmployees);
-    } else if (employeeInfo.length < (numOfEmployees.numOfManagers + numOfEmployees.numOfInterns)) {
-        createIntern(employeeInfo, numOfEmployees);
-    } else if (employeeInfo.length < (numOfEmployees.numOfManagers + numOfEmployees.numOfInterns + numOfEmployees.numOfEngineers)) {
-        createEngineer(employeeInfo, numOfEmployees);
-    } else {
-        let html =  render(employeeInfo);
-        fs.mkdir("output", function(err) {
-            // if (err) throw err;
-        });
-        fs.writeFile("output/team.html", html, function(err) {
-            if (err) throw err;
-        });
-    }
-}
-
-function createManager(employeeInfo, numOfEmployees){
-    inquirer.prompt([
-        {
-            message: "What is this Manager's name?",
-            name: "name",
-            type: "input"
-        },
-        {
-            message: "What is this Manager's email?",
-            name: "email",
-            type: "input"
-        },
-        {
-            message: "What is this Manager's office number?",
-            name: "officeNumber",
-            type: "input"
-        },
-    ]).then(answers => {
-        employeeInfo.push(new Manager(answers.name, employeeInfo.length, answers.email, answers.officeNumber));
-        createEmployeeInfo(employeeInfo, numOfEmployees);
-    });
-}
-
-function createIntern(employeeInfo, numOfEmployees){
-    inquirer.prompt([
-        {
-            message: "What is this Intern's name?",
-            name: "name",
-            type: "input"
-        },
-        {
-            message: "What is this Intern's email?",
-            name: "email",
-            type: "input"
-        },
-        {
-            message: "What is this Intern's school?",
-            name: "email",
-            type: "input"
-        },
-    ]).then(answers => {
-        employeeInfo.push(new Intern(answers.name, employeeInfo.length, answers.email, answers.school));
-        createEmployeeInfo(employeeInfo, numOfEmployees);
-    });
-}
-
-function createEngineer(employeeInfo, numOfEmployees){
-    inquirer.prompt([
-        {
-            message: "What is this Engineer's name?",
-            name: "name",
-            type: "input"
-        },
-        {
-            message: "What is this Engineer's email?",
-            name: "email",
-            type: "input"
-        },
-        {
-            message: "What is this Engineer's github username?",
-            name: "email",
-            type: "input"
-        },
-    ]).then(answers => {
-        employeeInfo.push(new Engineer(answers.name, employeeInfo.length, answers.email, answers.github));
-        createEmployeeInfo(employeeInfo, numOfEmployees);
-    });
-}
-
-generateEmployeeInfo();
+employeeInformation();
